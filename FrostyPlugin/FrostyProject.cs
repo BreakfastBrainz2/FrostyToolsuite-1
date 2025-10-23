7b723c773c2e38eeeaec4454cfa1d922a0ae29ef
Virjoinga <ethanbest51@gmail.com>
2025-10-24 11:55:58 +1300

    Reflection ID read and write (experimental)
    complex `RimeWidgetBlueprint` modding now should be possible
    
    https://github.com/J-Lyt/FrostyToolsuite/commit/4ae7db02d031af1f67838e331a1364be2a247db7
    
    https://github.com/J-Lyt/FrostyToolsuite/commit/6b8b4f8c193607c9d0ba2a76f9373630f210c88d

--- FrostyPlugin/FrostyProject.cs
+++ FrostyPlugin/FrostyProject.cs
@@ -290,6 +290,8 @@ namespace Frosty.Core
                         }
                         else
                         {
+                            Stream stream = App.AssetManager.GetEbxStream(entry, true);
+
                             // asset is using just regular data
                             EbxAsset asset = entry.ModifiedEntry.DataObject as EbxAsset;
                             using (EbxBaseWriter ebxWriter = EbxBaseWriter.CreateProjectWriter(new MemoryStream(), EbxWriteFlags.IncludeTransient))
