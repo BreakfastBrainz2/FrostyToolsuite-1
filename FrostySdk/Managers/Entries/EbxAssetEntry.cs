b960cdeba5d3160deab97750c89a98c75101a669
Virjoinga <ethanbest51@gmail.com>
2026-08-13 23:36:32 +1200

    Thought I reverted this already

--- FrostySdk/Managers/Entries/EbxAssetEntry.cs
+++ FrostySdk/Managers/Entries/EbxAssetEntry.cs
@@ -10,7 +10,7 @@ namespace FrostySdk.Managers.Entries
             get
             {
                 // TODO: @techdebt find better method to move blueprint bundles to sub-folder, this will most likely break writing.
-                if (ProfilesLibrary.IsLoaded(ProfileVersion.PlantsVsZombiesBattleforNeighborville) &&
+                if (ProfilesLibrary.IsLoaded(ProfileVersion.Battlefield2042) &&
                     (base.Name.StartsWith("cd_") || base.Name.StartsWith("md_") &! base.Name.Contains("win32/")))
                 {
                     return $"win32/{base.Name}";
